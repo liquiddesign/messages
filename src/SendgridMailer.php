d53af28aad862ea22f37c732e250ddab8d54a63a
Petr Křehlík <petr@lqd.cz>
2021-05-04 16:03:19 +0200

    sendgrid

--- src/SendgridMailer.php
+++ src/SendgridMailer.php
@@ -12,13 +12,13 @@ class SendgridMailer implements Mailer
 {
 	use SmartObject;
 
-    const ENDPOINT = "https://api.sendgrid.com/";
+	const ENDPOINT = "https://api.sendgrid.com/";
 
-    private string $key;
+	private string $key;
 
-    private string $tempFolder;
+	private string $tempFolder;
 
-    private array $tempFiles = [];
+	private array $tempFiles = [];
 
 	/**
 	 * MailSender constructor
@@ -26,80 +26,85 @@ class SendgridMailer implements Mailer
 	 * @param string $key
 	 * @param string $tempFolder
 	 */
-    public function __construct(string $key, string $tempFolder)
-    {
-        $this->key = $key;
-        $this->tempFolder = $tempFolder;
-    }
+	public function __construct(string $key, string $tempFolder)
+	{
+		$this->key = $key;
+		$this->tempFolder = $tempFolder;
+	}
 
 	/**
 	 * @param string $key
 	 */
-    public function setKey(string $key)
-    {
-        $this->key = $key;
-    }
+	public function setKey(string $key)
+	{
+		$this->key = $key;
+	}
 
 	/**
 	 * @param Message $message
 	 * @throws SendGrid\Exception
 	 */
-    public function send(Message $message):void
-    {
-        $sendGrid = new SendGrid($this->key);
-        $email = new Email();
+	public function send(Message $message): void
+	{
+		$sendGrid = new SendGrid($this->key);
+		$email = new Email();
 
-        $from = $message->getFrom();
-        reset($from);
-        $key = key($from);
+		$from = $message->getFrom();
+		reset($from);
+		$key = key($from);
 
-        $email->setFrom($key)
-            ->setFromName($from[$key])
-            ->setSubject($message->getSubject())
-            ->setText($message->getBody())
-            ->setHtml($message->getHtmlBody());
+		$email->setFrom($key)
+			->setFromName($from[$key])
+			->setSubject($message->getSubject())
+			->setText($message->getBody())
+			->setHtml($message->getHtmlBody())
+			->addHeader('dummy', '1');
 
-        foreach ($message->getAttachments() as $attachement) {
-            $header = $attachement->getHeader('Content-Disposition');
-            preg_match('/filename\=\"(.*)\"/', $header, $result);
-            $originalFileName = $result[1];
+		foreach ($message->getAttachments() as $attachement) {
+			$header = $attachement->getHeader('Content-Disposition');
+			preg_match('/filename\=\"(.*)\"/', $header, $result);
+			$originalFileName = $result[1];
 
-            $filePath = $this->saveTempAttachement($attachement->getBody());
+			$filePath = $this->saveTempAttachement($attachement->getBody());
 
-            $email->addAttachment($filePath, $originalFileName);
-        }
+			$email->addAttachment($filePath, $originalFileName);
+		}
 
-        foreach ($message->getHeader('To') as $recipient => $name) {
-            $email->addTo($recipient);
-        }
+		foreach ($message->getHeader('To') as $recipient => $name) {
+			$email->addTo($recipient);
+		}
 
-        foreach ($message->getHeader('Cc') as $recipient => $name) {
-            $email->addCc($recipient);
-        }
+		if ($message->getHeader('Cc')) {
+			foreach ($message->getHeader('Cc') as $recipient => $name) {
+				$email->addCc($recipient);
+			}
+		}
 
-        foreach ($message->getHeader('Bcc') as $recipient => $name) {
-            $email->addBcc($recipient);
-        }
+		if ($message->getHeader('Bcc')) {
+			foreach ($message->getHeader('Bcc') as $recipient => $name) {
+				$email->addBcc($recipient);
+			}
+		}
 
-        $sendGrid->send($email);
+		$sendGrid->send($email);
 
-        $this->cleanUp();
-    }
+		$this->cleanUp();
+	}
 
-    private function saveTempAttachement($body)
-    {
-        $filePath = $this->tempFolder . '/' . md5($body);
-        file_put_contents($filePath, $body);
-        array_push($this->tempFiles, $filePath);
+	private function saveTempAttachement($body)
+	{
+		$filePath = $this->tempFolder . '/' . md5($body);
+		file_put_contents($filePath, $body);
+		array_push($this->tempFiles, $filePath);
 
-        return $filePath;
-    }
+		return $filePath;
+	}
 
-    private function cleanUp()
-    {
-        foreach ($this->tempFiles as $file) {
-            unlink($file);
-        }
-    }
+	private function cleanUp()
+	{
+		foreach ($this->tempFiles as $file) {
+			unlink($file);
+		}
+	}
 
 }
