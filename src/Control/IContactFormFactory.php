bc46d7893b44708af2b51665a770c78a9c16bb80
Petr Krehlik <73128826+petr6@users.noreply.github.com>
2023-11-16 14:22:51 +0100

    fix: contact form factory type

--- src/Control/IContactFormFactory.php
+++ src/Control/IContactFormFactory.php
@@ -4,9 +4,7 @@ declare(strict_types=1);
 
 namespace Messages\Control;
 
-use Forms\Form;
-
 interface IContactFormFactory
 {
-	public function create(): Form;
+	public function create(): ContactForm;
 }
